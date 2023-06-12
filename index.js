0fb0f4aaaabab74e10c813f2317a6b3ecfa343b3
Usha Sai Chintha <ushasai154@gmail.com>
2023-06-13 01:45:08 +0530

    ensuring data being loaded automatically via nodemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// importing ApolloServer and gql (graphql) from apollo-server
 const { ApolloServer, gql } = require('apollo-server')
 
+// Make changes to below data and the server gets automatically refreshed with data changes if index.js is run via nodemon
 const books = [
     {
       id: 1,
@@ -11,6 +11,11 @@ const books = [
       id: 2,
       title: 'City of Glass',
       author: 'Paul Auster',
+    },
+    {
+      id: 3,
+      title: 'RRR',
+      author: 'Rajamouli',
     },
 ];
 
@@ -27,49 +32,18 @@ const typeDefs = gql`
         book(id: ID!): Book
     }
 `
-// resolvers take in the request and work on resolving. It kind of middleware in processing
 const resolvers = {
     Query: {
         books: () => books,
-//        book: (parent, args, context, info) => books.find(book => book.id == args.id)
-        // above line works same as below line
         book: (_, { id }) => books.find(book => book.id == id)
     }
 }
 
-// creating an ApolloServer
 const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({url}) => {
     console.log(`Server is running at ${url}`)
 })
 
-// to run the server command is
-////  node index.js
-// Above command shows a message saying server started at running port 4000 which is a default port
-
-// Now we can launch http://localhost:4000/ url and run query server.. where in we can create our queries
-
-
-// Queries -
-
-//// 1. All books
-
-//query ExampleQuery {
-//  books {
-//    id
-//    title
-//    author
-//  }
-//}
-
-
-//// 2. Search a book via an ID
-
-//query ExampleQuery {
-//  book (id: 1) {
-//    id
-//    title
-//    author
-//  }
-//}
+// to run nodemon, the command is
+////  nodemon index.js
